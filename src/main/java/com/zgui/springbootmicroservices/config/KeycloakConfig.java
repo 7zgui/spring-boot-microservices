d7622b7ede066d6d8ece27b37ab1475d4f8acecf
ouokki <ouokki.7zgui@gmail.com>
2022-09-14 21:56:45 +0100

    keycloack-implemented

---
+++ src/main/java/com/zgui/springbootmicroservices/config/KeycloakConfig.java
@@ -0,0 +1,19 @@
+package com.zgui.springbootmicroservices.config;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * @author Mohamed ouokki
+ */
+
+@Configuration
+public class KeycloakConfig {
+
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+}
